54707ff02b91336db6c4670efc7488468d2b8814
rahmahaisyah <rahmahasyh@gmail.com>
2026-06-11 04:37:19 +0700

    step 1: setup continuous integration (compile)

---
+++ src/main/java/Counter.java
@@ -0,0 +1,25 @@
+public class Counter {
+
+	private int count;
+	
+	public Counter() {
+		this.reset();
+	}
+		
+	public void reset() {
+		count = 0;
+	}
+	
+	public void increment() {
+		count++;
+	}
+	
+	public void decrement() {
+		count--;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+}
